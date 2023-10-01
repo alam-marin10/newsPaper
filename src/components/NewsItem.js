5ca6a072449168f45df48bf6f1e92284ee573c08
Mahbubul Alam Marin <alammarin10@outlook.com>
2023-10-01 12:04:48 -0600

    newspap

---
+++ src/components/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+  render() {
+    let { title, description, urlToImage ,url} = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card" style={{ width:"18rem"}}>
+          <img src={!urlToImage?"https://deadline.com/wp-content/uploads/2023/08/WGA-Strike-1.jpg?w=1024" : urlToImage } className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}..</h5>
+            <p className="card-text">{description}...</p>
+            <a href={url} className="btn btn-dark btn-sm">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
